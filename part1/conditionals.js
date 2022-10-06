6a80224b0071201005cb312c4e79086b96971aa4
bskumur <bskumur@hotmail.com>
2022-10-06 22:33:06 +0300

    part1

---
+++ part1/conditionals.js
@@ -0,0 +1,17 @@
+var not = 40
+
+if(not > 50){
+    console.log( "Geçtiniz")
+}
+
+// if(not <= 50){
+//     console.log( "Kaldınız")
+// }
+
+else if (not>= 40){
+    console.log(" Bütünlemeye Kaldınız")
+}
+
+else{
+    console.log("Kaldınız")
+}
